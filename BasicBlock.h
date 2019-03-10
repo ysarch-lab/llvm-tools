120796956c68aff636781577e7d25e390aaaa353
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-10 19:57:12 -0400

    BasicBlock: Add successor BB iterator.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- BasicBlock.h
+++ BasicBlock.h
@@ -36,5 +36,49 @@ public:
 	inst_iterator end()
 	{ return inst_iterator(bb_, nullptr); }
 
+	const ::std::string getName()
+	{ return ::std::string(LLVMGetBasicBlockName(bb_)); }
+
+	class successor_iterator {
+		LLVMValueRef bb_term_;
+		unsigned successor_id_;
+	public:
+		successor_iterator(LLVMBasicBlockRef &bb, int successor = 0):bb_term_(LLVMGetBasicBlockTerminator(bb)), successor_id_(successor)
+		{
+			if (successor == -1)
+				successor_id_ = LLVMGetNumSuccessors(bb_term_);
+		}
+
+		bool operator != (const successor_iterator &i)
+		{ return successor_id_ != i.successor_id_; }
+
+		const successor_iterator & operator++()
+		{ ++successor_id_; return *this; }
+
+
+		BasicBlock operator *()
+		{
+			auto ref = succ_ref();
+			return BasicBlock(ref);
+		}
+
+		::std::unique_ptr<BasicBlock> operator -> ()
+		{
+			auto ref = succ_ref();
+			return ::std::unique_ptr<BasicBlock>(new BasicBlock(ref));
+		}
+
+	private:
+		LLVMBasicBlockRef succ_ref() {
+			return LLVMGetSuccessor(bb_term_, successor_id_);
+		}
+	};
+
+	successor_iterator successor_begin()
+	{ return successor_iterator(bb_); }
+
+	successor_iterator successor_end()
+	{ return successor_iterator(bb_, -1); }
+
 	friend ::std::ostream & operator << (::std::ostream &, const BasicBlock &);
 };
