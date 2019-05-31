cd5ee1eb6dda52dc6b80a5e1746d245469e38983
Jan Vesely <jan.vesely@rutgers.edu>
2019-05-30 21:13:53 -0400

    extract_model_code: Preserve both run and exec wrappers
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- extract_model_code.sh
+++ extract_model_code.sh
@@ -6,8 +6,10 @@ echo "Running on '$D'"
 MODEL_NAME=`echo $D | sed 's%/%_%g' | sed 's/models_//'`
 echo "Model name: '$MODEL_NAME'"
 
-FUNCTION=`grep define "$D"/*.parse.ll | grep -o '[^"]*exec[^"]*'`
-echo "Function: $FUNCTION"
+EXEC_FUNCTION=`grep define "$D"/*.parse.ll | grep -o '[^"]*exec_wrap[^"]*'`
+echo "Execute function: $EXEC_FUNCTION"
+RUN_FUNCTION=`grep define "$D"/*.parse.ll | grep -o '[^"]*run_wrap[^"]*'`
+echo "Run function: $RUN_FUNCTION"
 
 if [ "x$LLVM_PREFIX" == "x" ]; then
 	LLVM_PREFIX=/usr/local/llvm-git
@@ -18,4 +20,4 @@ LLVM_OPT=$LLVM_PREFIX/bin/opt
 
 # Link all .parse.ll files.
 # Inline all functions, and internalize all but the call to exec_*.
-$LLVM_LINK "$D"/*.parse.ll | $LLVM_OPT -always-inline -internalize -internalize-public-api-list=$FUNCTION -globaldce -stats -S -o "$MODEL_NAME.ll"
+$LLVM_LINK "$D"/*.parse.ll | $LLVM_OPT -forceattrs -force-attribute="$EXEC_FUNCTION:alwaysinline" -always-inline -internalize -internalize-public-api-list=$EXEC_FUNCTION,$RUN_FUNCTION -globaldce -stats -S -o "$MODEL_NAME.ll"
