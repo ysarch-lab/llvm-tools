e11b601ab4becc9367e19357a3caaa518f5b53e7
Jan Vesely <jan.vesely@rutgers.edu>
2019-06-16 19:25:08 -0400

    result-range: Add utility to display known ranges of values stored to output
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

---
+++ result-range.cpp
@@ -0,0 +1,93 @@
+#include <getopt.h>
+
+#include <cassert>
+#include <deque>
+#include <iostream>
+#include <string>
+
+#include <llvm/Analysis/Passes.h>
+#include <llvm/Analysis/LazyValueInfo.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Support/SourceMgr.h>
+
+static const struct option options[] = {
+	{"help", required_argument, NULL, 'h'},
+};
+
+using val_list = ::std::deque<::std::pair<::llvm::Value*, ::llvm::Instruction*>>;
+
+static void get_stored_values(::llvm::Value *val, val_list &stored_vals) {
+	if (::llvm::StoreInst *i = ::llvm::dyn_cast<::llvm::StoreInst>(val)) {
+		::std::cerr << "Used in store: ";
+		i->dump();
+		stored_vals.push_back(::std::make_pair(i->getValueOperand(), i));
+
+	}
+	for (auto u:val->users())
+		get_stored_values(u, stored_vals);
+}
+
+static val_list find_result_values(::llvm::Function &f) {
+	// Run analysis pass on function
+	::std::cerr << "Running on function: " << f.getName().str() << "\n";
+	::llvm::Argument &output_arg = f.arg_begin()[4];
+	::std::cerr << "Output argument: ";
+	output_arg.dump();
+	val_list vals;
+	get_stored_values(&output_arg, vals);
+	return vals;
+}
+
+int main(int argc, char **argv) {
+	char c = -1;
+	while ((c = getopt_long(argc, argv, "h", options, NULL)) != -1) {
+		switch (c) {
+		default:
+			::std::cerr << "Unknown option: " << argv[optind - 1]
+			            << ::std::endl;
+		case 'h':
+			::std::cerr << "Available options:\n";
+			return c == 'h' ? 0 : 1;
+		}
+	}
+
+	static ::llvm::LLVMContext context;
+	::std::deque<::std::unique_ptr<::llvm::Module>> modules;
+	for (int i = optind; i < argc; ++i) {
+		::std::cout << "Parsing file: " << argv[i] << "\n";
+		::llvm::SMDiagnostic error;
+		modules.push_back(::llvm::parseIRFile(argv[i], error, context));
+	}
+
+	static const ::std::string func_name = "run_";
+	for (auto &m:modules) {
+		for (auto &f: m->functions()) {
+			auto mm = ::std::mismatch(func_name.begin(),
+			                          func_name.end(),
+						  f.getName().begin());
+			if (mm.first == func_name.end()) {
+				::llvm::legacy::PassManager pm;
+				::llvm::Pass *p = ::llvm::createLazyValueInfoPass();
+				pm.add(p);
+				pm.run(*m);
+				::llvm::LazyValueInfoWrapperPass *wp =
+					(::llvm::LazyValueInfoWrapperPass *)p;
+				auto &lvi = wp->getLVI();
+				for (auto v:find_result_values(f)) {
+					lvi.getConstantRange(v.first, v.second->getParent()).dump();
+					::std::cerr << " is known about: ";
+					v.first->dump();
+				}
+				wp->releaseMemory();
+			}
+		}
+	}
+
+	return 0;
+}
